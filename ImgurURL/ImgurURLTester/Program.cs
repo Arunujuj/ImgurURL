f567b8c89b32068476bc90dc37ddcc9ce7fcacf1
Arunujuj <arunujuj@gmail.com>
2019-05-04 21:59:44 +0200

    tester update
    - check for availability of url
    - retry if url not available

--- ImgurURL/ImgurURLTester/Program.cs
+++ ImgurURL/ImgurURLTester/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,15 +15,48 @@ namespace ImgurURLTester
 
         static void Main(string[] args)
         {
+            Console.WriteLine("MAX URLS: " + URL_COUNT);
             for(int i = 0; i < URL_COUNT; i++)
             {
                 ImgurURL.ImgurURL test = new ImgurURL.ImgurURL();
-                Console.WriteLine(test.GetImgurURL(rnd.Next(999999999).ToString()));
+                string url = test.GetImgurURL(rnd.Next(999999999).ToString());
+
+                switch(CheckURLAvailable(url))
+                {
+                    case true:
+                        Console.WriteLine(i + ": " + url);
+                        break;
+                    case false:
+                        i--;
+                        break;
+                }
             }
-
-
-            
             Console.ReadLine();
+        }
+
+        static bool CheckURLAvailable(string url)
+        {
+            bool isAvailable = false;
+
+            using (WebClient test = new WebClient())
+            {
+                string fullURL = "https://www.imgur.com/" + url;
+
+                try
+                {
+                    var html = test.DownloadString(fullURL);
+                    isAvailable = true;
+
+                }
+                catch(WebException ex)
+                {
+                    isAvailable = false;
+                }
+
+                
+
+            }
+            return isAvailable;
         }
     }
 }
