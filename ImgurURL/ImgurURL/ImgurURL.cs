8f636c2d31c1f037c7d24b1e4a177c295f7d2dc5
Arunujuj <arunujuj@gmail.com>
2019-05-04 21:46:43 +0200

    Init Project
    - added library
    - convert ulong to base62 notation (borrowed from the webz)
    - test application

---
+++ ImgurURL/ImgurURL/ImgurURL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurURL
+{
+    public class ImgurURL
+    {
+        public static string URL_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private string ToBase62(ulong number)
+        {
+            var alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var n = number;
+            ulong basis = 62;
+            var ret = "";
+            while (n > 0)
+            {
+                ulong temp = n % basis;
+                ret = alphabet[(int)temp] + ret;
+                n = (n / basis);
+
+            }
+            return ret;
+        }
+
+        public string GetImgurURL(string number)
+        {
+            return ToBase62(Convert.ToUInt32(number));
+        }
+
+    }
+}
---
+++ ImgurURL/ImgurURLTester/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgurURLTester
+{
+    class Program
+    {
+        static int URL_COUNT = 10;
+        static Random rnd = new Random();
+    
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < URL_COUNT; i++)
+            {
+                ImgurURL.ImgurURL test = new ImgurURL.ImgurURL();
+                Console.WriteLine(test.GetImgurURL(rnd.Next(999999999).ToString()));
+            }
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
